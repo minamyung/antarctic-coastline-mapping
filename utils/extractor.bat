78c55e1be295c17436b27b5d3605dc729c3d4acd
Manon Myung <53259548+minamyung@users.noreply.github.com>
2021-09-17 14:04:33 +0100

    Update extractor.bat

--- utils/extractor.bat
+++ utils/extractor.bat
@@ -1,4 +1,5 @@
 @echo off
 @REM Unzips all files in the Coastline_images_to_extract folder into the Coastline_images folder. Delete all files in the first folder after running to avoid duplicates.
-FOR %%i IN (C:\Users\myung\Documents\CSC8099\Data\Coastline_images_to_extract\*) DO tar -xvzf %%i -C C:\Users\myung\Documents\CSC8099\Data\Coastline_images
+@REM Add source folder and destination folder paths below
+FOR %%i IN (C:\*) DO tar -xvzf %%i -C C:\
 ECHO Done
