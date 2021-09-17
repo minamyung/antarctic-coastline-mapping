bd61476897b2d5cea76ce7bad9ee5262698bb6a6
Manon Myung <myung.mina@gmail.com>
2021-09-17 15:24:24 +0100

    updated test_import and requirements

--- utils/test_import.py
+++ utils/test_import.py
@@ -1,20 +1,25 @@
-# Tests the required imports for the sake of my sanity
+# Tests the required imports
 # Recommended: use conda and conda-forge to install the necessary packages
-# Use a conda env (in this case acm) to run the code
-# Make sure that VScode recognises conda and the env is correctly activated!!!!!!!!!!!!!!
+# Use a conda env to run the code
+
 
 from osgeo import gdal
 import cv2
 import numpy as np
 import scipy
 import matplotlib
-import tifffile as tifi
-
+import geopandas as gpd
+import rasterio
+import affine
+import shapely
 
 assert(gdal.__version__ is not None), "GDAL import failed"
 assert(cv2.__version__ is not None), "cv2 import failed"
 assert(np.__version__ is not None), "numpy import failed"
 assert(scipy.__version__ is not None), "scipy import failed"
 assert(matplotlib.__version__ is not None), "matplotlib import failed"
-assert(tifi.__version__ is not None), "tifffile import failed"
+assert(gpd.__version__ is not None), "matplotlib import failed"
+assert(rasterio.__version__ is not None), "matplotlib import failed"
+assert(affine.__version__ is not None), "matplotlib import failed"
+assert(shapely.__version__ is not None), "tifffile import failed"
 print("Imports OK.")
